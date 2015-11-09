5bce374322833eafa438724c1b4d2b6119959299
Jane Sternbach <janesternbach@gmail.com>
2015-11-08 22:11:16 -0500

    travis with feature specs?

--- spec/features/start_game_feature_spec.rb
+++ spec/features/start_game_feature_spec.rb
@@ -3,8 +3,7 @@ require "spec_helper"
 describe "Start Game", js: true do
   include LoginHelper
 
-
-  context 'when a user is logged in' do
+  context "when a user is logged in" do
     let(:user) { create(:user) }
     let(:score_board) { page.find_by_id("pageScoreboard") }
     let(:computer_score) { score_board.find_by_id("computer_score") }
@@ -21,16 +20,16 @@ describe "Start Game", js: true do
       page.click_link("New Game")
     end
 
-    it 'starts with computer and player score 0' do
+    it "starts with computer and player score 0" do
       expect(computer_score).to have_text("0")
       expect(player_score).to have_text("0")
     end
 
-    context 'starting movies' do
+    context "starting movies" do
       let(:movie_title) { page.find_by_id("movie_title", visible: false) }
       let(:movie_tmdb_id) { page.find_by_id("movie_tmdb", visible: false) }
 
-      it 'loads a starting movie to start' do
+      it "loads a starting movie to start" do
         expect(movie_title.text).to_not be_blank
         expect(starting_movies.map(&:tmdb_id)).to include(movie_tmdb_id.text.to_i)
       end
