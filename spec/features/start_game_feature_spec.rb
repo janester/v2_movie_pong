703a09ee881e68101eeb85fb9b06876dd1303c38
Jane Sternbach <janesternbach@gmail.com>
2016-02-21 01:24:14 -0800

    feature specs for starting actors

--- spec/features/start_game_feature_spec.rb
+++ spec/features/start_game_feature_spec.rb
@@ -9,13 +9,23 @@ describe "Start Game", js: true do
     let(:computer_score) { score_board.find_by_id("computer_score") }
     let(:player_score) { score_board.find_by_id("player_score") }
     let(:starting_movies) { (1..2).map { create(:movie, :as_starting_movie) } }
+
     let(:create_movies) do
       starting_movies
       2.times { create(:movie) }
     end
 
+    let(:create_actors) do
+      Movie.all.map do |m|
+        2.times { m.actors << create(:actor) }
+      end
+    end
+
+    let(:new_movie_btn) { page.find_by_id("new_movie_btn") }
+    let(:entered_actor_btn) { page.find_by_id("entered_actor_btn") }
+
     before do
-      create_movies
+      create_movies && create_actors
       sign_in_user(user)
       page.click_link("New Game")
     end
@@ -25,6 +35,14 @@ describe "Start Game", js: true do
       expect(player_score).to have_text("0")
     end
 
+    it "shows the ask for a new movie button" do
+      expect(new_movie_btn.visible?).to eq true
+    end
+
+    it "shows the entered actor button" do
+      expect(new_movie_btn.visible?).to eq true
+    end
+
     context "starting movies" do
       let(:movie_title) { page.find_by_id("movie_title", visible: false) }
       let(:movie_tmdb_id) { page.find_by_id("movie_tmdb", visible: false) }
@@ -32,6 +50,20 @@ describe "Start Game", js: true do
       it "loads a starting movie to start" do
         expect(movie_title.text).to_not be_blank
         expect(starting_movies.map(&:tmdb_id)).to include(movie_tmdb_id.text.to_i)
+      end
+    end
+
+    context "actors" do
+      let(:actor_items) { page.all("a.ui-corner-all") }
+      let(:starting_actors) { Actor.pluck("name") }
+
+      before do
+        fill_in("entered_actor", with: "h")
+        sleep 1
+      end
+
+      it "lists all the actors" do
+        expect(actor_items.map(&:text)).to eq starting_actors
       end
     end
   end
